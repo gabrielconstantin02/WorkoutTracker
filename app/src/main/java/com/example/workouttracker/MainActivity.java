79ded87a7598b5cb54796260f3fc2e5720395307
Kira060200 <constantin.gab02@gmail.com>
2019-02-10 20:24:11 +0200

    Updated UI

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -74,12 +74,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -87,10 +82,9 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handle navigation item
         int id = item.getItemId();
 
         if (id == R.id.nav_workout) {
--- app/src/main/java/com/example/workouttracker/Workout.java
+++ app/src/main/java/com/example/workouttracker/Workout.java
@@ -1,12 +1,109 @@
 package com.example.workouttracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class Workout extends AppCompatActivity {
+public class Workout extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        /*SharedPreferences preferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        int myNumber;
+        myNumber = preferences.getInt("myNumber", 0);
+        myNumber++;
+        editor.putInt("myNumber", myNumber);
+        editor.commit();
+        TextView errorView = (TextView) findViewById(R.id.title);
+        errorView.setText(String.valueOf(myNumber));*/
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation item
+        int id = item.getItemId();
+
+        if (id == R.id.nav_workout) {
+            Intent intent = new Intent(this, Workout.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_health) {
+            Intent intent = new Intent(this, Health.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_food) {
+            Intent intent = new Intent(this, Food.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_stats) {
+            Intent intent = new Intent(this, Stats.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_manage) {
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(this, Share.class);
+            startActivity(intent);
+            finish();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
