4b272eda450598d70d2737f82d2581d8274c94a4
kira02 <constantin.gab02@gmail.com>
2019-03-22 21:45:21 +0200

    Implemented back to menu button all over the app

--- app/src/main/java/com/example/workouttracker/Food.java
+++ app/src/main/java/com/example/workouttracker/Food.java
@@ -1,12 +1,19 @@
 package com.example.workouttracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Food extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food);
+    }
+    public void onBackPressed(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/workouttracker/Health.java
+++ app/src/main/java/com/example/workouttracker/Health.java
@@ -1,12 +1,20 @@
 package com.example.workouttracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class Health extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.health);
+    }
+    public void onBackPressed(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
