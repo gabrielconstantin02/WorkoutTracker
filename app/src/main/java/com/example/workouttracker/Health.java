61dd13a2acd00fbe90dace76ce5da6888b1dfdae
Constantin Gabriel <constantin.gab02@gmail.com>
2019-08-17 13:06:37 +0300

    Implemented water intake function

--- app/src/main/java/com/example/workouttracker/Health.java
+++ app/src/main/java/com/example/workouttracker/Health.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,6 +22,8 @@ public class Health extends AppCompatActivity {
         setContentView(R.layout.health);
         preferences = getSharedPreferences("my_pref", MODE_PRIVATE);
         editor = preferences.edit();
+        Thread dateThread = new Thread(new checkDate());
+        dateThread.start();
 
     }
     public void onBackPressed(View view) {
@@ -28,24 +31,33 @@ public class Health extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-    public void checkDate(){
-        Date c = Calendar.getInstance().getTime();
-        System.out.println("Current time => " + c);
+    public class checkDate implements Runnable{
+        public void run() {
+            while (true) {
+                Date c = Calendar.getInstance().getTime();
+                System.out.println("Current time => " + c);
 
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        String formattedDate = df.format(c);
-        String myDate=null;
-        try {
-            myDate = df.format(preferences.getString("myDate", "defaultValue"));
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        if (formattedDate.equals(myDate)){
-            //increment water intake per day and active time
-        }
-        else{
-            preferences .edit().putString("myDate", formattedDate).apply();
-            //save this values to another category like weekly water intake
+                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+                String formattedDate = df.format(c);
+                String myDate = null;
+                try {
+                    myDate = df.format(preferences.getString("myDate", "defaultValue"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                EditText qt = (EditText) findViewById(R.id.input_water);
+                try {
+                    Double quantity = Double.valueOf(qt.getText().toString());
+                    if (formattedDate.equals(myDate)) {
+                        TextView pqt = (TextView) findViewById(R.id.water_today);
+                        //increment water intake per day and active time
+                    } else {
+                        preferences.edit().putString("myDate", formattedDate).apply();
+                        //save this values to another category like weekly water intake
+                    }
+                }catch (Exception ex){
+                }
+            }
         }
     }
 }
