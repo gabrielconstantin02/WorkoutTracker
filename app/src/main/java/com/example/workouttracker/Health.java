4c59a30eaccfdba999fa8b32ed1127fd4da8dc51
Constantin Gabriel <constantin.gab02@gmail.com>
2019-08-18 10:57:17 +0300

    Water stats implemented

--- app/src/main/java/com/example/workouttracker/Health.java
+++ app/src/main/java/com/example/workouttracker/Health.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,42 +24,60 @@ public class Health extends AppCompatActivity {
         setContentView(R.layout.health);
         preferences = getSharedPreferences("my_pref", MODE_PRIVATE);
         editor = preferences.edit();
-        Thread dateThread = new Thread(new checkDate());
-        dateThread.start();
-
+        TextView mqt=(TextView) findViewById(R.id.water_month);
+        float water_month=preferences.getFloat("water month",0);
+        mqt.setText(String.valueOf(water_month));
+        TextView pqt = (TextView) findViewById(R.id.water_today);
+        float water_today=preferences.getFloat("water today",0);
+        pqt.setText(String.valueOf(water_today));
     }
     public void onBackPressed(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-    public class checkDate implements Runnable{
-        public void run() {
-            while (true) {
+    public void onDone(View view)  {
                 Date c = Calendar.getInstance().getTime();
                 System.out.println("Current time => " + c);
 
                 SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
                 String formattedDate = df.format(c);
                 String myDate = null;
+                Log.d("CurrentDate",formattedDate);
                 try {
-                    myDate = df.format(preferences.getString("myDate", "defaultValue"));
+                    myDate = preferences.getString("myDate", "16-Aug-2019");
+                    Log.d("SavedDate",myDate);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
                 EditText qt = (EditText) findViewById(R.id.input_water);
                 try {
-                    Double quantity = Double.valueOf(qt.getText().toString());
-                    if (formattedDate.equals(myDate)) {
-                        TextView pqt = (TextView) findViewById(R.id.water_today);
+                    Float quantity = Float.valueOf(qt.getText().toString());
+                    Log.d("water",String.valueOf(quantity));
+                    TextView pqt = (TextView) findViewById(R.id.water_today);
+                    float water_today=preferences.getFloat("water today",0);
+                    //pqt.setText(String.valueOf(water));
+                    if (formattedDate.equals(myDate)||myDate.equals("16-Aug-2019")) {
+                        //Float water_today=Float.valueOf(pqt.getText().toString());
+                        Log.d("water",String.valueOf(water_today));
+                        water_today=quantity+water_today;
+                        editor.putFloat("water today",water_today);
+                        pqt.setText(String.valueOf(water_today));
                         //increment water intake per day and active time
                     } else {
-                        preferences.edit().putString("myDate", formattedDate).apply();
+                        editor.putString("myDate", formattedDate);
+                        editor.commit();
                         //save this values to another category like weekly water intake
                     }
+                    TextView mqt=(TextView) findViewById(R.id.water_month);
+                    float water_month=preferences.getFloat("water month",0);
+                    Log.d("month",String.valueOf(water_month));
+                    Float total=water_month+quantity;
+                    mqt.setText(String.valueOf(total));
+                    editor.putFloat("water month",total);
+                    editor.commit();
+                    qt.setText(null);
                 }catch (Exception ex){
                 }
             }
         }
-    }
-}
