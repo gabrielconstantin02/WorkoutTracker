1e9c3869018f7374ccb8963ff6a119f1c7a3079c
Constantin Gabriel <constantin.gab02@gmail.com>
2019-08-18 11:35:22 +0300

    Water stats remastered

--- app/src/main/java/com/example/workouttracker/Health.java
+++ app/src/main/java/com/example/workouttracker/Health.java
@@ -18,18 +18,23 @@ import java.util.Date;
 public class Health extends AppCompatActivity {
     SharedPreferences preferences ; //Save key-value data
     SharedPreferences.Editor editor;    //Edit key-value data
+    int day=1;
+    float total_quantity=0;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.health);
         preferences = getSharedPreferences("my_pref", MODE_PRIVATE);
         editor = preferences.edit();
+        total_quantity=preferences.getFloat("water per day",0);
         TextView mqt=(TextView) findViewById(R.id.water_month);
         float water_month=preferences.getFloat("water month",0);
-        mqt.setText(String.valueOf(water_month));
+        mqt.setText("water consumed this month:"+water_month);
         TextView pqt = (TextView) findViewById(R.id.water_today);
         float water_today=preferences.getFloat("water today",0);
-        pqt.setText(String.valueOf(water_today));
+        pqt.setText("Water consumed today:"+water_today);
+        TextView tqt=(TextView) findViewById(R.id.water_per_day);
+        tqt.setText("Water consumed/day:"+total_quantity/day);
     }
     public void onBackPressed(View view) {
         Intent intent = new Intent(this, MainActivity.class);
@@ -53,6 +58,7 @@ public class Health extends AppCompatActivity {
                 EditText qt = (EditText) findViewById(R.id.input_water);
                 try {
                     Float quantity = Float.valueOf(qt.getText().toString());
+                    total_quantity+=quantity;
                     Log.d("water",String.valueOf(quantity));
                     TextView pqt = (TextView) findViewById(R.id.water_today);
                     float water_today=preferences.getFloat("water today",0);
@@ -62,20 +68,27 @@ public class Health extends AppCompatActivity {
                         Log.d("water",String.valueOf(water_today));
                         water_today=quantity+water_today;
                         editor.putFloat("water today",water_today);
-                        pqt.setText(String.valueOf(water_today));
+                        pqt.setText("Water consumed today:"+String.valueOf(water_today));
                         //increment water intake per day and active time
                     } else {
                         editor.putString("myDate", formattedDate);
                         editor.commit();
+                        pqt.setText("Water consumed today:"+String.valueOf(quantity));
+                        editor.putFloat("water today",quantity);
+                        editor.commit();
+                        day++;
                         //save this values to another category like weekly water intake
                     }
                     TextView mqt=(TextView) findViewById(R.id.water_month);
                     float water_month=preferences.getFloat("water month",0);
                     Log.d("month",String.valueOf(water_month));
                     Float total=water_month+quantity;
-                    mqt.setText(String.valueOf(total));
+                    mqt.setText("Water consumed this month:"+String.valueOf(total));
                     editor.putFloat("water month",total);
                     editor.commit();
+                    TextView tqt=(TextView) findViewById(R.id.water_per_day);
+                    editor.putFloat("water per day",total_quantity);
+                    tqt.setText("Water consumed/day:"+String.valueOf(total_quantity/day));
                     qt.setText(null);
                 }catch (Exception ex){
                 }
